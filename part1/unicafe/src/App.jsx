08252a665bcfb9df4a183a3a1a1f6238fb83508f
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-06-12 10:46:34 +0100

    Finish exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,9 +6,10 @@ function Button({ onClick, text }) {
 
 function StatisticLine({ text, value }) {
   return (
-    <p>
-      {text} {value}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 }
 
@@ -22,14 +23,16 @@ function Statistics({ good, neutral, bad }) {
   }
 
   return (
-    <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={`${positive}%`} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={`${positive}%`} />
+      </tbody>
+    </table>
   );
 }
 
