2c35025b417261412a2fcafeda8f1a87c5555bca
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-06-12 10:31:51 +0100

    Finish exercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,10 @@ function Statistics({ good, neutral, bad }) {
   const average = (good * 1 + neutral * 0 + bad * -1) / all;
   const positive = (good / all) * 100;
 
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return <p>No feedback given</p>;
+  }
+
   return (
     <div>
       <p>good {good}</p>
