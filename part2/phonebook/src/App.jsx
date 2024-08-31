00fe2f37716a411819792bb5172baf206d429629
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-08-31 09:48:17 +0100

    Finish exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
+    if (!newName || !newNumber) {
+      alert("Please make sure all fields are filled");
+      return;
+    }
     const nameInPersons = persons.find(
       (p) => p.name.toLowerCase() === newName.toLowerCase()
     );
@@ -15,13 +22,19 @@ const App = () => {
     }
     const newPerson = {
       name: newName,
+      number: newNumber,
     };
     setPersons((person) => [...person, newPerson]);
     setNewName("");
+    setNewNumber("");
   };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -33,12 +46,18 @@ const App = () => {
           <input type="text" value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number:{" "}
+          <input type="text" value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   );
