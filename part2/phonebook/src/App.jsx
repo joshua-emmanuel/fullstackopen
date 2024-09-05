eb737ea7dd538a2d8a14b9b0ab85daea66d03c5c
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-09-05 16:47:50 +0100

    Finish exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import SearchFilter from "./components/SearchFilter";
 import PersonForm from "./components/PersonForm";
 import PeopleList from "./components/PeopleList";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
@@ -17,6 +13,13 @@ const App = () => {
   const filteredPeople = persons.filter((person) =>
     person.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      const persons = response.data;
+      setPersons(persons);
+    });
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
