44d4e77f56f226893e165fbdb7e2e548cf69690a
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2025-05-12 05:15:09 +0100

    Finish exercise 2.20

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -23,13 +23,23 @@ export default function App() {
   const [query, setQuery] = useState("");
   const debouncedQuery = useDebouncedQuery(query, 1000);
   const [result, setResult] = useState(null);
+  const [capitalWeather, setCapitalWeather] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+  const API_KEY = import.meta.env.VITE_API_KEY;
 
   useEffect(
     function () {
       const controller = new AbortController();
       const signal = controller.signal;
+
+      async function getWeatherData(city) {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+        );
+        const weatherData = await res.json();
+        return weatherData;
+      }
 
       async function findCountry() {
         try {
@@ -54,6 +64,9 @@ export default function App() {
             );
 
           if (filteredCountries.length === 1) {
+            const countryCapital = filteredCountries[0].capital[0];
+            const weatherData = await getWeatherData(countryCapital);
+            setCapitalWeather([weatherData]);
             setResult(filteredCountries);
             return;
           }
@@ -63,7 +76,7 @@ export default function App() {
           );
           setResult(countryNames);
         } catch (error) {
-          // To ignore signal was aborted without reason error when user types fast and query isn't debounced
+          /* To ignore signal was aborted without reason error when user types fast and query isn't debounced */
           // if (error.name === "AbortError") return;
           setError(error.message);
         } finally {
@@ -113,11 +126,12 @@ export default function App() {
           : result?.length === 1 && (
               <>
                 <div>
-                  <h2>{result[0].name.common}</h2>
-                  <p>capital {result[0].capital}</p>
-                  <p>area {result[0].area}</p>
+                  <h1>{result[0].name.common}</h1>
+                  <span>Capital {result[0].capital}</span>
+                  <br />
+                  <span>Area {result[0].area}</span>
 
-                  <h3>languages:</h3>
+                  <h3>Languages</h3>
                   <ul>
                     {Object.values(result[0]?.languages).map((language) => (
                       <li key={language}>{language}</li>
@@ -125,6 +139,16 @@ export default function App() {
                   </ul>
 
                   <img src={result[0].flags.png} alt={result[0].flags.alt} />
+                </div>
+
+                <div>
+                  <h2>Weather in {capitalWeather[0].name}</h2>
+                  <p>Temperature {capitalWeather[0].main.temp} celsius</p>
+                  <img
+                    src={`https://openweathermap.org/img/wn/${capitalWeather[0].weather[0].icon}@2x.png`}
+                    alt={`${capitalWeather[0].weather[0].description} icon`}
+                  />
+                  <p>Wind {capitalWeather[0].wind.speed}m/s</p>
                 </div>
               </>
             )}
